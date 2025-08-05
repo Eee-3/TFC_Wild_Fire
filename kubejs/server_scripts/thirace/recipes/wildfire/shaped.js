65d5983146b9945dfbc1655a42735d348684e743
Thirteen <55902963+Thirace446@users.noreply.github.com>
2025-08-05 18:54:48 +0800

    Update shaped.js

--- kubejs/server_scripts/thirace/recipes/wildfire/shaped.js
+++ kubejs/server_scripts/thirace/recipes/wildfire/shaped.js
@@ -1,74 +1,27 @@
 ServerEvents.recipes((event) => {
 
-    const id_prefix = 'kubejs:thirace/shaped/';
+    const id_prefix = 'kubejs:thirace/wildfire/shaped/';
 
     const recipes = [
 
-/*         {
-            output: 'minecraft:stonecutter',
+        {
+            output: 'createmetallurgy:sturdy_whisk',
             pattern: [
                 ' A ',
-                ' B ',
-                '   ',],
+                'BAB',
+                'BBB',],
             key: {
-                A: Item.of('immersiveengineering:sawblade', '{Damage:0}'),
-                B: 'create:andesite_casing'
+                A: '#forge:ingots/steel',
+                B: 'create:sturdy_sheet'
             },
-            id: `${id_prefix}stonecutter`
-        }, */
+            id: `${id_prefix}sturdy_whisk`
+        },
 
     ];
-
-/*     const woods = [
-        'acacia',
-        'ash',
-        'aspen',
-        'birch',
-        'blackwood',
-        'chestnut',
-        'douglas_fir',
-        'hickory',
-        'kapok',
-        'mangrove',
-        'maple',
-        'oak',
-        'palm',
-        'pine',
-        'rosewood',
-        'sequoia',
-        'spruce',
-        'sycamore',
-        'white_cedar',
-        'willow'
-    ]
-
-    woods.forEach((wood) => {
-        recipes.push({
-            output: `tfc:wood/boat/${wood}`,
-            pattern: [
-                'ABA',
-                'AAA',
-                '   ',],
-            key: {
-                A: `tfc:wood/lumber/${wood}`,
-                B: '#forge:slimeballs'
-            },
-            id: `${id_prefix}boat/${wood}`
-        })
-    }); */
-
-
 
 
     recipes.forEach((recipe) => {
         event.shaped(recipe.output, recipe.pattern, recipe.key).id(recipe.id);
     });
 });
-
-
-
-
-
-
-
 
