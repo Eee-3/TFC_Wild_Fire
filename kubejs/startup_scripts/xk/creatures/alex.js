a4bb27f00a0bad4e5031314524a8bfcdb66c7b6d
Thirteen <55902963+Thirace446@users.noreply.github.com>
2025-08-11 00:37:03 +0800

    witzig-heidi

--- kubejs/startup_scripts/xk/creatures/alex.js
+++
@@ -1,8 +0,0 @@
-TFCEvents.registerFaunas(e => {
-    e.replace("alexsmobs:tiger", "on_ground", "world_surface")
-    e.replace("alexsmobs:snow_leopard", "on_ground", "world_surface")
-    e.replace("alexsmobs:orca", "in_water", "ocean_floor")
-    e.replace("alexsmobs:crocodile", "in_water", "ocean_floor")
-
-     e.replace("untamedwilds:bear", "on_ground", "world_surface")
-})
