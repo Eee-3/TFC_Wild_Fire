0df1dd119ddea80493c6aa63b6749bcce874d206
xiankuang <a1369505035a@gmail.com>
2025-08-28 10:22:00 +0800

    物品大小以及重量

--- kubejs/server_scripts/tfc/size_modified.js
+++ kubejs/server_scripts/tfc/size_modified.js
@@ -1,11 +1,11 @@
 TFCEvents.data(e => {
-  const small_light_items=[
+  const small_light_items = [
     'cuisinedelight:suspicious_mix',
     'cuisinedelight:ham_fried_rice',
     'cuisinedelight:fried_rice',
     'cuisinedelight:mixed_fried_rice',
     'cuisinedelight:meat_with_seafood',
-    'cuisinedelight:meat_with_vegetables', 
+    'cuisinedelight:meat_with_vegetables',
     'cuisinedelight:seafood_with_vegetables',
     'cuisinedelight:fried_pasta',
     'cuisinedelight:mixed_pasta',
@@ -22,8 +22,59 @@ TFCEvents.data(e => {
     'cuisinedelight:fried_meat_and_melon',
     'cuisinedelight:scrambled_egg_and_tomato',
   ]
-  small_light_items.forEach(item=>{
+  small_light_items.forEach(item => {
     e.itemSize(item, 'small', 'light')
   })
+  const huge_very_heavy_items = [
+    "immersive_aircraft:phull",
+    "immersive_aircraft:engine",
+    "immersive_aircraft:boiler",
+    "immersive_aircraft:airship",
+    "immersive_aircraft:cargo_airship",
+    "immersive_aircraft:warship",
+    "immersive_aircraft:biplane",
+    "immersive_aircraft:gyrodyne",
+    "immersive_aircraft:quadrocopter",
+    "immersive_aircraft:rotary_cannon",
+    "immersive_aircraft:heavy-crossbow",
+    "immersive_aircraft:telescope",
+    "immersive_aircraft:bomb_bay",
+    "immersive_aircraft:enhanced_propeller",
+    "immersive_aircraft:eco_engine",
+    "immersive_aircraft:nether_engine",
+    "immersive_aircraft:steel_boiler",
+    "immersive_aircraft:hull_reinforcement",
+    "siegemachines:mortar",
+    "siegemachines:culverin",
+    "siegemachines:catapult",
+    "siegemachines:trebuchet",
+    "siegemachines:ballista",
+    "siegemachines:battering-ram",
+    "siegemachines:siege_ladder",
+    "siegemachines:cannonball",
+    "siegemachines:barrel"]
+  huge_very_heavy_items.forEach(item => {
+    e.itemSize(item, 'huge', 'very_heavy')
+
+  })
+
+  const large_heavy_items = [
+    "siegemachines:turret_base",
+    "siegemachines:beam",
+    "siegemachines:giant_arrow"
+  ]
+
+  large_heavy_items.forEach(item => {
+    e.itemSize(item, 'large', 'heavy')
+
+  })
+
+
+
+
+
+
+
 
 })
+
