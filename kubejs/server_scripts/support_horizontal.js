0161406f4fcf30b336fa5a8bf16fdc275aeb927e
witzig-heidi <heidiedos@gmail.com>
2025-09-05 10:43:09 +0800

    铜与钢支撑梁模型贴图

--- kubejs/server_scripts/support_horizontal.js
+++ kubejs/server_scripts/support_horizontal.js
@@ -4,4 +4,9 @@ ServerEvents.blockLootTables(event =>{
             pool.addItem("kubejs:copper_support")
         })
     })
+    event.addBlock("kubejs:steel_support_horizontal", loot=>{
+        loot.addPool(pool=>{
+            pool.addItem("kubejs:steel_support")
+        })
+    })
 })
--- kubejs/startup_scripts/zcl/support.js
+++ kubejs/startup_scripts/zcl/support.js
@@ -1,4 +1,6 @@
 StartupEvents.registry('block', event => {    
     event.create('kubejs:copper_support', 'tfc:support')
+.tagBlock('minecraft:mineable/pickaxe')    
+    event.create('kubejs:steel_support', 'tfc:support')
 .tagBlock('minecraft:mineable/pickaxe')
 })
