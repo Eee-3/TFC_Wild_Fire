a276627d0e8c52c5ad9e3a83d3c6584759b957bc
witzig-heidi <heidiedos@gmail.com>
2025-09-11 01:15:01 +0800

    更新金属梁韧性、更新异种石头坍塌属性

---
+++ kubejs/server_scripts/collapse&landslide.js
@@ -0,0 +1,108 @@
+ServerEvents.tags('block', event => {
+    //金星石
+    event.add('tfc:can_start_collapse', 'ad_astra:venus_stone')
+    event.add('tfc:can_trigger_collapse', 'ad_astra:venus_stone')
+    event.add('tfc:can_collapse', 'ad_astra:venus_stone')
+
+    event.add('tfc:can_start_collapse', 'ad_astra:venus_coal_ore')
+    event.add('tfc:can_trigger_collapse', 'ad_astra:venus_coal_ore')
+    event.add('tfc:can_collapse', 'ad_astra:venus_coal_ore')
+
+    event.add('tfc:can_start_collapse', 'ad_astra:venus_calorite_ore')
+    event.add('tfc:can_trigger_collapse', 'ad_astra:venus_calorite_ore')
+    event.add('tfc:can_collapse', 'ad_astra:venus_calorite_ore')
+
+    event.add('tfc:can_start_collapse', 'ad_astra:venus_diamond_ore')
+    event.add('tfc:can_trigger_collapse', 'ad_astra:venus_diamond_ore')
+    event.add('tfc:can_collapse', 'ad_astra:venus_diamond_ore')
+
+    event.add('tfc:can_start_collapse', 'ad_astra:venus_gold_ore')
+    event.add('tfc:can_trigger_collapse', 'ad_astra:venus_gold_ore')
+    event.add('tfc:can_collapse', 'ad_astra:venus_gold_ore')
+
+    event.add('tfc:can_landslide', 'ad_astra:venus_cobblestone')
+    //月石
+    event.add('tfc:can_start_collapse', 'ad_astra:moon_stone')
+    event.add('tfc:can_trigger_collapse', 'ad_astra:moon_stone')
+    event.add('tfc:can_collapse', 'ad_astra:moon_stone')
+
+    event.add('tfc:can_start_collapse', 'ad_astra:moon_deepslate')
+    event.add('tfc:can_trigger_collapse', 'ad_astra:moon_deepslate')
+    event.add('tfc:can_collapse', 'ad_astra:moon_deepslate')
+
+    event.add('tfc:can_landslide', 'ad_astra:moon_cobblestone')
+
+    //砾岩
+    event.add('tfc:can_start_collapse', 'ad_astra:conglomerate')
+    event.add('tfc:can_trigger_collapse', 'ad_astra:conglomerate')
+    event.add('tfc:can_collapse', 'ad_astra:conglomerate')
+
+    //火星石
+    event.add('tfc:can_start_collapse', 'ad_astra:mars_stone')
+    event.add('tfc:can_trigger_collapse', 'ad_astra:mars_stone')
+    event.add('tfc:can_collapse', 'ad_astra:mars_stone')
+
+    event.add('tfc:can_start_collapse', 'ad_astra:mars_iron_ore')
+    event.add('tfc:can_trigger_collapse', 'ad_astra:mars_iron_ore')
+    event.add('tfc:can_collapse', 'ad_astra:mars_iron_ore')
+
+    event.add('tfc:can_start_collapse', 'ad_astra:mars_diamond_ore')
+    event.add('tfc:can_trigger_collapse', 'ad_astra:mars_diamond_ore')
+    event.add('tfc:can_collapse', 'ad_astra:mars_diamond_ore')
+
+    event.add('tfc:can_start_collapse', 'ad_astra:mars_ostrum_ore')
+    event.add('tfc:can_trigger_collapse', 'ad_astra:mars_ostrum_ore')
+    event.add('tfc:can_collapse', 'ad_astra:mars_ostrum_ore')
+
+    event.add('tfc:can_start_collapse', 'ad_astra:mars_ice_shard_ore')
+    event.add('tfc:can_trigger_collapse', 'ad_astra:mars_ice_shard_ore')
+    event.add('tfc:can_collapse', 'ad_astra:mars_ice_shard_ore')
+
+    event.add('tfc:can_landslide', 'ad_astra:mars_cobblestone')
+
+    //水星石
+    event.add('tfc:can_start_collapse', 'ad_astra:mercury_stone')
+    event.add('tfc:can_trigger_collapse', 'ad_astra:mercury_stone')
+    event.add('tfc:can_collapse', 'ad_astra:mercury_stone')
+
+    event.add('tfc:can_start_collapse', 'ad_astra:mercury_iron_ore')
+    event.add('tfc:can_trigger_collapse', 'ad_astra:mercury_iron_ore')
+    event.add('tfc:can_collapse', 'ad_astra:mercury_iron_ore')
+
+    event.add('tfc:can_landslide', 'ad_astra:mercury_cobblestone')
+
+    //深板岩
+    event.add('tfc:can_start_collapse', 'minecraft:deepslate')
+    event.add('tfc:can_trigger_collapse', 'minecraft:deepslate')
+    event.add('tfc:can_collapse', 'minecraft:deepslate')
+
+    event.add('tfc:can_start_collapse', 'ad_astra:deepslate_ostrum_ore')
+    event.add('tfc:can_trigger_collapse', 'ad_astra:deepslate_ostrum_ore')
+    event.add('tfc:can_collapse', 'ad_astra:deepslate_ostrum_ore')
+
+    event.add('tfc:can_start_collapse', 'ad_astra:deepslate_calorite_ore')
+    event.add('tfc:can_trigger_collapse', 'ad_astra:deepslate_calorite_ore')
+    event.add('tfc:can_collapse', 'ad_astra:deepslate_calorite_ore')
+
+    event.add('tfc:can_start_collapse', 'ad_astra:deepslate_desh_ore')
+    event.add('tfc:can_trigger_collapse', 'ad_astra:deepslate_desh_ore')
+    event.add('tfc:can_collapse', 'ad_astra:deepslate_desh_ore')
+
+    event.add('tfc:can_start_collapse', 'ad_astra:deepslate_ice_shard_ore')
+    event.add('tfc:can_trigger_collapse', 'ad_astra:deepslate_ice_shard_ore')
+    event.add('tfc:can_collapse', 'ad_astra:deepslate_ice_shard_ore')
+
+    event.add('tfc:can_landslide', 'minecraft:cobbled_deepslate')
+
+    //玄武岩、黑石
+    event.add('tfc:can_start_collapse', 'minecraft:basalt')
+    event.add('tfc:can_trigger_collapse', 'minecraft:basalt')
+    event.add('tfc:can_collapse', 'minecraft:basalt')
+
+    event.add('tfc:can_start_collapse', 'minecraft:blackstone')
+    event.add('tfc:can_trigger_collapse', 'minecraft:blackstone')
+    event.add('tfc:can_collapse', 'minecraft:blackstone')
+
+    event.add('tfc:can_landslide', 'tfc:rock/cobble/basalt')
+ 
+})
---
+++ kubejs/server_scripts/zcl.js
@@ -0,0 +1,25 @@
+ServerEvents.tags('block', event => {
+    event.add('tfc:toughness_1', 'kubejs:copper_support') // 铜
+    event.add('tfc:toughness_1', 'kubejs:copper_support_horizontal')
+    event.add('tfc:toughness_1', 'kubejs:bismuth_bronze_support') // 铋铜
+    event.add('tfc:toughness_1', 'kubejs:bismuth_bronze_support_horizontal')
+    event.add('tfc:toughness_1', 'kubejs:bronze_support') // 青铜
+    event.add('tfc:toughness_1', 'kubejs:bronze_support_horizontal')
+    event.add('tfc:toughness_1', 'kubejs:black_bronze_support') // 黑铜
+    event.add('tfc:toughness_1', 'kubejs:black_bronze_support_horizontal')
+    event.add('tfc:toughness_2', 'kubejs:pig_iron_support') // 铸铁
+    event.add('tfc:toughness_2', 'kubejs:pig_iron_support_horizontal')
+    event.add('tfc:toughness_2', 'kubejs:wrought_iron_support') // 锻铁
+    event.add('tfc:toughness_2', 'kubejs:wrought_iron_support_horizontal')
+    event.add('tfc:toughness_2', 'kubejs:steel_support') // 钢
+    event.add('tfc:toughness_2', 'kubejs:steel_support_horizontal')
+    event.add('tfc:toughness_3', 'kubejs:black_steel_support') // 黑钢
+    event.add('tfc:toughness_3', 'kubejs:black_steel_support_horizontal')
+    event.add('tfc:toughness_3', 'kubejs:galvanized_steel_support') // 镀锌方钢
+    event.add('tfc:toughness_3', 'kubejs:galvanized_steel_support_horizontal')
+    event.add('tfc:toughness_3', 'kubejs:red_steel_support') // 红钢
+    event.add('tfc:toughness_3', 'kubejs:red_steel_support_horizontal')
+    event.add('tfc:toughness_3', 'kubejs:blue_steel_support') // 蓝钢
+    event.add('tfc:toughness_3', 'kubejs:blue_steel_support_horizontal')
+
+})
