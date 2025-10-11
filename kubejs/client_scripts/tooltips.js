92478bdb3b772c37e540a3f7e0294d7032651219
witzig-heidi <heidiedos@gmail.com>
2025-10-11 13:56:35 +0800

    汉化订正、结构修改[未完成]

--- kubejs/client_scripts/tooltips.js
+++ kubejs/client_scripts/tooltips.js
@@ -2,10 +2,10 @@
 let tooltips = [
     ["minecraft:dirt", "原版泥土无法获取，玻璃罐建议使用沃土", "Dirt is not obtainable, use the rich soil instead"],
     ["tfc_ie_addon:mineral/quartz_shard", "石英块可以拆", "Quartz shard can be dismantled"],
-    ["kubejs:alumina_dioxide", "§7在§r§e2980°C§r下熔化成 100mb 铝", "Fluids can be melted at §e2980°C§r to 100mb of aluminium"],
+    ["kubejs:alumina_dioxide", "§7在§r§e2980°C§r温度下熔化成 100mb 铝", "Fluids can be melted at §e2980°C§r to 100mb of aluminium"],
     ["createmetallurgy:industrial_crucible", "§4此物品已禁用，且可能会导致崩溃§r", "This item is disabled and may cause a crash."],
     ["eidolon:enchanted_ash", "§8“试试洒在地上围成一个圈，它能驱散亡灵使其无法靠近”§r", "§8“Try sprinkling it on the ground to form a circle! It can repel the undead and keep them from approaching.”§r"],
-    ["#tfc:water_wheels", "§8“这种水车最好在有流动的§r§7河水§r§8处使用”§r", "§8“This kind of water wheel is best used where there is flowing §r§7river water§r§8.”§r"]
+    ["#tfc:water_wheels", "§8“这种水车需要在流动的§r§7河水§r§8处使用”§r", "§8“This kind of water wheel is best used where there is flowing §r§7river water§r§8.”§r"]
 ]
 tooltips.forEach(([key, zh_cn, en_us]) => {
     // 添加key
