38c8fbdf2886ca3902be07633e414201e4119ac1
Hanabi <a1369505035a@gmail.com>
2025-11-05 22:22:08 +0800

    进一步酿酒相关配方修订&模组更新

--- kubejs/startup_scripts/fluid.js
+++ kubejs/startup_scripts/fluid.js
@@ -6,4 +6,64 @@ StartupEvents.registry("fluid",event=>{
     event.create("kubejs:light_oil")
     .thickTexture(0X261A15)
     .noBlock()
+
+    event.create("kubejs:beer_maize")
+    .stillTexture('kubejs:block/beer_maize')
+    .flowingTexture('kubejs:block/beer_maize_flowing')
+
+    event.create("kubejs:beer_barley")
+    .stillTexture('kubejs:block/beer_barley')
+    .flowingTexture('kubejs:block/beer_barley_flowing')
+    
+    event.create("kubejs:beer_hops")
+    .stillTexture('kubejs:block/beer_hops')
+    .flowingTexture('kubejs:block/beer_hops_flowing')
+    
+    event.create("kubejs:beer_rye")
+    .stillTexture('kubejs:block/beer_rye')
+    .flowingTexture('kubejs:block/beer_rye_flowing')
+    
+    event.create("kubejs:beer_oat")
+    .stillTexture('kubejs:block/beer_oat')
+    .flowingTexture('kubejs:block/beer_oat_flowing')
+    
+    event.create("kubejs:beer_haley")
+    .stillTexture('kubejs:block/beer_haley')
+    .flowingTexture('kubejs:block/beer_haley_flowing')
+    
+    event.create("kubejs:whiskey_jojannik")
+    .stillTexture('kubejs:block/whiskey_jojannik')
+    .flowingTexture('kubejs:block/whiskey_jojannik_flowing')
+    
+    event.create("kubejs:whiskey_lilitusinglemalt")
+    .stillTexture('kubejs:block/whiskey_ilitusinglemalt')
+    .flowingTexture('kubejs:block/whiskey_ilitusinglemalt_flowing')
+        
+    event.create("kubejs:whiskey_cristelwalker")
+    .stillTexture('kubejs:block/whiskey_cristelwalker')
+    .flowingTexture('kubejs:block/whiskey_cristelwalker_flowing')
+        
+    event.create("kubejs:whiskey_maggoallan")
+    .stillTexture('kubejs:block/whiskey_maggoallan')
+    .flowingTexture('kubejs:block/whiskey_maggoallan_flowing')
+        
+    event.create("kubejs:whiskey_carrasconlabel")
+    .stillTexture('kubejs:block/whiskey_carrasconlabel')
+    .flowingTexture('kubejs:block/whiskey_carrasconlabel_flowing')
+        
+    event.create("kubejs:whiskey_ak")
+    .stillTexture('kubejs:block/whiskey_ak')
+    .flowingTexture('kubejs:block/whiskey_ak_flowing')
+        
+    event.create("kubejs:whiskey_highland_hearth")
+    .stillTexture('kubejs:block/whiskey_highland_hearth')
+    .flowingTexture('kubejs:block/whiskey_highland_hearth_flowing')
+            
+    event.create("kubejs:whiskey_smokey_reverie")
+    .stillTexture('kubejs:block/whiskey_smokey_reverie')
+    .flowingTexture('kubejs:block/whiskey_smokey_reverie_flowing')
+            
+    event.create("kubejs:whiskey_jamesons_malt")
+    .stillTexture('kubejs:block/whiskey_jamesons_malt')
+    .flowingTexture('kubejs:block/whiskey_jamesons_malt_flowing')
 })
