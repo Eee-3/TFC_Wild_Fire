9afa25edbc6f550e28cb8955e48ad3aeac9105fc
JasonQ <nmuqindexiang@163.com>
2025-11-08 01:36:48 +0800

    增加注释

--- kubejs/server_scripts/health.js
+++ kubejs/server_scripts/health.js
@@ -5,7 +5,9 @@ const $BodyPartEnum = Java.loadClass("sfiomn.legendarysurvivaloverhaul.api.bodyd
  * @param {number} time 单位是s
  */
 function cooldown(event, time){
+  //设置冷却为1，防止重复触发
   wait = 1
+  //计时器计时time秒后，将冷却设为0
   event.server.scheduleInTicks(time * 20, func => {
     wait = 0
   })
@@ -15,8 +17,10 @@ function cooldown(event, time){
  * @param {Internal.LivingEntityHurtEventJS} e 
  */
 function adrenaline(e){
+  //首先给予玩家painkiller以及speed效果，持续600tick
   e.entity.potionEffects.add("legendarysurvivaloverhaul:painkiller", 600)
   e.entity.potionEffects.add("minecraft:speed", 300)
+  //600tick后，给予玩家slowness以及darkness效果，持续100tick
   e.server.scheduleInTicks(600, func => {
     e.entity.potionEffects.add("minecraft:slowness", 60, 9)
     e.entity.potionEffects.add("minecraft:darkness", 100)
@@ -40,8 +44,11 @@ PlayerEvents.tick(e =>{
   }
 })
 //肾上腺素
+// 当玩家血量低于20%时，有p的概率触发肾上腺素
 let p = 0.05
+// 判断是否冷却中
 let wait = 0
+//复活及登录时清除冷却，删除肾上腺素显示
 PlayerEvents.loggedIn(e => {
   wait = 0
   e.player.paint({adrenaline: {remove: true}})
@@ -49,27 +56,39 @@ PlayerEvents.loggedIn(e => {
 PlayerEvents.respawned(e => {
   wait = 0
   e.player.paint({adrenaline: {remove: true}})
-  loop.clear()
 })
+// 当玩家受伤时，判断是否触发肾上腺素
 EntityEvents.hurt(e => {
   const { entity, player } = e
-  if(entity.isPlayer() && (entity.getHealth() / entity.getMaxHealth() < 0.1) && wait == 0) {
+  //如果是玩家，且血量低于20%，且冷却中为0时进行一次判定
+  if(entity.isPlayer() && (entity.getHealth() / entity.getMaxHealth() < 0.2) && wait == 0) {
+    //每次判定固定增加0.1，最大增加到0.95
     p = Math.min(p + 0.1, 0.95)
+    //进行随机判定，若小于p，则触发肾上腺素
     if (Math.random() < p) {
+      //触发肾上腺素后，重置p为0.05
       p = 0.05
+      //触发肾上腺素后，设置冷却为60s
       cooldown(e, 60)
+      //触发肾上腺素效果
       adrenaline(e)
+      //显示肾上腺素提示
       player.setStatusMessage(Text.translatable("message.kubejs.adrenaline"))
+      //显示肾上腺素覆盖层
       player.paint({adrenaline: {type: 'atlas_texture', x: 0, y: 0, alignX: 'center', alignY: 'center', w: "$screenW", h: "$screenH", draw: "always", texture: "kubejs:item/adrenaline"}})
+      //每20ticks播放一次心跳音效
       let loop = e.server.scheduleRepeatingInTicks(20, func => {
         player.sendData("kubejs_player_playsound", {soundEvent: "minecraft:entity.warden.heartbeat", volume: 1.0, pitch: 0.8})
       })
+      //600tick后，删除肾上腺素覆盖层以及关闭心跳声
       e.server.scheduleInTicks(600, func => {
         player.paint({adrenaline: {remove: true}})
         loop.clear()
       })
+      //取消本次伤害
       e.cancel()
     } else if(wait != 1) {
+      //若未触发肾上腺素，且冷却中为0时，设置冷却为60s
       cooldown(e, 60)
     }
   }
