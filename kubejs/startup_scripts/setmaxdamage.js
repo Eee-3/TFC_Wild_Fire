72cee132b8b5eec6d32643409ca425cf00b33fb5
框框 <a1369505035a@gmail.com>
2025-11-20 18:22:16 +0800

    大锤耐久翻倍

--- kubejs/startup_scripts/setmaxdamage.js
+++ kubejs/startup_scripts/setmaxdamage.js
@@ -1,15 +1,38 @@
 ItemEvents.modification(event => {
 
+
+
+    const itema = [
+        { name: 'tfc_hammer_time:metal/sledgehammer/copper', num: 1200 },//挖矿锤
+        { name: 'tfc_hammer_time:metal/sledgehammer/bismuth_bronze', num: 2400 },
+        { name: 'tfc_hammer_time:metal/sledgehammer/black_bronze', num: 3000 },
+        { name: 'tfc_hammer_time:metal/sledgehammer/black_bronze', num: 2600 },
+        { name: 'tfc_hammer_time:metal/sledgehammer/wrought_iron', num: 4500 },
+        { name: 'tfc_hammer_time:metal/sledgehammer/steel', num: 6600 },
+        { name: 'tfc_hammer_time:metal/sledgehammer/black_steel', num: 9000 },
+        { name: 'tfc_hammer_time:metal/sledgehammer/red_steel', num: 15000 },
+        { name: 'tfc_hammer_time:metal/sledgehammer/blue_steel', num: 15000 },
+
+
+
+    ]
+    itema.forEach(itema => {
+        event.modify(itema.name, modify => { modify.setMaxDamage(itema.num) })
+    })
+
+
+
+
     event.modify('artisanal:stone/flint_and/pyrite', modify => {
         modify.setMaxDamage(20)
     })//黄铁打火石
-     event.modify('create:extendo_grip', modify => {
+    event.modify('create:extendo_grip', modify => {
         modify.setMaxDamage(800)
     })//黄铁打火石
     event.modify('create:super_glue', modify => {
         modify.setMaxDamage(500)
     })//强力胶
-      event.modify('bsa:bone/fishing_rod', modify => {
+    event.modify('bsa:bone/fishing_rod', modify => {
         modify.setMaxDamage(50)
     })//骨头吊杆
     const attack = [
