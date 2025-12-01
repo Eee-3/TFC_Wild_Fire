22dbadbfe4dfdef099425e281f78caf0a1420e8b
witzig-heidi <heidiedos@gmail.com>
2025-12-01 20:00:37 +0800

    我也不知道我改了啥反正就是上传

--- kubejs/client_scripts/ttt.js
+++ kubejs/client_scripts/ttt.js
@@ -1,15 +1,14 @@
 // client
-// zh_cn "tooltip.hot":"&c烫烫烫"
+// zh_cn "tooltip.hot":"§c烫烫烫"
 ItemEvents.tooltip(event => {
     event.addAdvancedToAll((item, advanced, text) => {
-
-        if((TFC.misc.getHeat(item).getTemperature() > 300)&&(TFC.misc.getHeat(item).getTemperature() < 1500)){
-            text.add(Text.translate('tooltip.hot'))
+        if(TFC.misc.getHeat(item) != null){
+            if(TFC.misc.getHeat(item).getTemperature() > 300){
+                text.add(Text.translate('tooltip.hot'))
+            }
         }
-    })
-    event.addAdvancedToAll((item, advanced, text) => {
-        if(TFC.misc.getHeat(item).getTemperature() > 1499){
-            text.add(Text.translate('tooltip2.hot'))
+        else if(item.hasTag('kubejs:hot_items')){
+            text.add(Text.translate('tooltip.hot'))
         }
     })
 })
--- kubejs/server_scripts/tags.js
+++ kubejs/server_scripts/tags.js
@@ -42,7 +42,7 @@ ServerEvents.tags("item", event => {
     )
 
 
-   event.add('kubejs:tongs', 'create:extendo_grip')//机械臂
+    event.add('kubejs:tongs', 'create:extendo_grip')//机械臂
 
 
 
@@ -50,8 +50,62 @@ ServerEvents.tags("item", event => {
     // 将手套加入进饰品tag
     event.add('kubejs:tongs', 'kubejs:wooden_tong')
     event.add('curios:hands', 'kubejs:glove')
-    // 熔岩桶是热的物品
-    event.add('kubejs:hot_items', 'minecraft:lava_bucket')
+    // 熔岩桶和熔融桶是热的物品
+    event.add('kubejs:hot_items',
+        [
+            'minecraft:lava_bucket',
+            "firmalife:bucket/metal/chromium",
+            "firmalife:bucket/metal/stainless_steel",
+            "tfc_ie_addon:bucket/metal/electrum",
+            "tfc_ie_addon:bucket/metal/constantan",
+            "tfc_ie_addon:bucket/metal/aluminum",
+            "tfc_ie_addon:bucket/metal/lead",
+            "tfc_ie_addon:bucket/metal/uranium",
+            "tfc:bucket/metal/bismuth",
+            "tfc:bucket/metal/bismuth_bronze",
+            "tfc:bucket/metal/black_bronze",
+            "tfc:bucket/metal/bronze",
+            "tfc:bucket/metal/brass",
+            "tfc:bucket/metal/copper",
+            "tfc:bucket/metal/gold",
+            "tfc:bucket/metal/nickel",
+            "tfc:bucket/metal/rose_gold",
+            "tfc:bucket/metal/silver",
+            "tfc:bucket/metal/tin",
+            "tfc:bucket/metal/zinc",
+            "tfc:bucket/metal/sterling_silver",
+            "tfc:bucket/metal/weak_blue_steel",
+            "tfc:bucket/metal/red_steel",
+            "tfc:bucket/metal/weak_steel",
+            "tfc:bucket/metal/blue_steel",
+            "tfc:bucket/metal/black_steel",
+            "tfc:bucket/metal/steel",
+            "tfc:bucket/metal/pig_iron",
+            "tfc:bucket/metal/cast_iron",
+            "tfc:bucket/metal/wrought_iron",
+            "tfc:bucket/metal/weak_red_steel",
+            "tfc:bucket/metal/high_carbon_steel",
+            "tfc:bucket/metal/high_carbon_black_steel",
+            "tfc:bucket/metal/high_carbon_blue_steel",
+            "tfc:bucket/metal/high_carbon_red_steel",
+            "tfc:bucket/metal/unknown"
+        ])
+
+    // 灼热的物品
+    event.add('kubejs:hot_items',
+        [
+            "minecraft:magma_block",
+            "alexscaves:primal_magma",
+            "tfc:rock/magma/basalt",
+            "tfc:rock/magma/granite",
+            "tfc:rock/magma/diorite",
+            "tfc:rock/magma/rhyolite",
+            "tfc:rock/magma/andesite",
+            "tfc:rock/magma/dacite",
+            "alexscaves:volcanic_core",
+            "scguns:vehement_coal_block",
+            "scguns:vehement_coal"
+        ])
 
 })
 const registerAsticorCartsItemTags = (event) => {
