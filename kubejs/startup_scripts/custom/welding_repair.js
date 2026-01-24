a1f304eaf3d92b37e385e8bd70a1c1e1fbf4eeaf
框框 <a1369505035a@gmail.com>
2026-01-24 19:23:54 +0800

    焊接bug修一下

--- kubejs/startup_scripts/custom/welding_repair.js
+++ kubejs/startup_scripts/custom/welding_repair.js
@@ -2,55 +2,61 @@ TFCEvents.registerItemStackModifier(event => {
 
     event.withInput('kubejs:welding_repair_percentage15', (output, input) => {
         if (input.id == output.id) {
-            let { damageValue, maxDamage } = input
+            let ci = input.copy()
+            let { damageValue, maxDamage } = ci
             let damage = Math.max(0, damageValue - maxDamage * 0.15)
-            output.setDamageValue(damage)
-            return output
+            ci.setDamageValue(damage)
+            return ci
         }
         return input
     })
     event.withInput('kubejs:welding_repair_percentage10', (output, input) => {
         if (input.id == output.id) {
-            let { damageValue, maxDamage } = input
+            let ci = input.copy()
+            let { damageValue, maxDamage } = ci
             let damage = Math.max(0, damageValue - maxDamage * 0.1)
-            output.setDamageValue(damage)
-            return output
+            ci.setDamageValue(damage)
+            return ci
         }
         return input
     })
     event.withInput('kubejs:welding_repair_percentage7.5', (output, input) => {
         if (input.id == output.id) {
-            let { damageValue, maxDamage } = input
+              let ci = input.copy()
+            let { damageValue, maxDamage } = ci
             let damage = Math.max(0, damageValue - maxDamage * 0.075)
-            output.setDamageValue(damage)
-            return output
+            ci.setDamageValue(damage)
+            return ci
         }
         return input
     })
     event.withInput('kubejs:welding_repair_percentage5', (output, input) => {
         if (input.id == output.id) {
-            let { damageValue, maxDamage } = input
+             let ci = input.copy()
+            let { damageValue, maxDamage } = ci
             let damage = Math.max(0, damageValue - maxDamage * 0.05)
-            output.setDamageValue(damage)
-            return output
+            ci.setDamageValue(damage)
+            return ci
         }
         return input
     })
     event.withInput('kubejs:welding_repair_percentage4', (output, input) => {
         if (input.id == output.id) {
-            let { damageValue, maxDamage } = input
+               let ci = input.copy()
+            let { damageValue, maxDamage } = ci
             let damage = Math.max(0, damageValue - maxDamage * 0.04)
-            output.setDamageValue(damage)
-            return output
+            ci.setDamageValue(damage)
+            return ci
         }
         return input
     })
      event.withInput('kubejs:welding_repair_percentage2', (output, input) => {
         if (input.id == output.id) {
-            let { damageValue, maxDamage } = input
+            let ci = input.copy()
+            let { damageValue, maxDamage } = ci
             let damage = Math.max(0, damageValue - maxDamage * 0.02)
-            output.setDamageValue(damage)
-            return output
+            ci.setDamageValue(damage)
+            return ci
         }
         return input
     })
